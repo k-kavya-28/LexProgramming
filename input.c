826b52accbb75aaf6a4b350348dc9f307ebaecee
Khushi Kavya <khushi.kavya6910@gmail.com>
2023-09-04 12:45:50 +0530

    compiler

--- input.c
+++ input.c
@@ -1,19 +1,14 @@
-/* C program to illustrate use of single line and multi-line comment */
+
 #include <stdio.h>
 int main(void)
 {
-	/*
+	// single line comment 
+/*
 	This is a
 	multi-line comment
-	*/
+*/
+	// single line comment 
 
-	/*
-	This comment contains some code which
-	will not be executed.
-	printf("Code enclosed in Comment");
-	*/
-
-    // single line comment 
 	printf("Boom");
 	return 0;
 }
